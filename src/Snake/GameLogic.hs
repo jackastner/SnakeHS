c7597248a8af6ef6864652ce02679a0826c31e20
John Kastner <john.h.kastner@gmail.com>
2016-06-02 10:48:42 -0400

    Changed Gamelogic to use Vectors instead of tuple points

--- src/Snake/GameLogic.hs
+++ src/Snake/GameLogic.hs
@@ -1,44 +1,53 @@
-module Snake where 
+{-# LANGUAGE FlexibleContexts #-}
+module Snake.GameLogic where 
 
 import Data.List
 import System.Random
-import Control.Applicative
 
-data SnakeGame = SnakeGame {snake :: Snake,
-                            goal :: Point,
-                            snakeDir :: Direction,
+import Control.Lens
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+
+import Linear.Affine
+import Linear.V2
+
+data SnakeGame a = SnakeGame {snake :: [Point V2 a],
+                            goal :: Point V2 a ,
+                            snakeDir :: V2 a,
                             score :: Int,
                             gameOver :: Bool,
-                            bounds :: Rectangle,
+                            bounds :: (Point V2 a,Point V2 a),
                             rng :: StdGen}
 
-type Point = (Int,Int)
-type Rectangle = (Point,Point)
-type Snake = [Point]
-type Direction = (Int,Int)
-
-instance (Random a0, Random a1) => Random (a0,a1) where
-    randomR ((x0,y0),(x1,y1)) g = ((x,y),g'')
-        where (x,g')  = randomR (x0,x1) g
-              (y,g'') = randomR (y0,y1) g'
-    random g = ((x,y),g'')
+instance Random a => Random (V2 a) where
+    randomR (l,u) g = (V2 x y,g'')
+        where (x,g')  = randomR (l^._x,u^._x) g
+              (y,g'') = randomR (l^._y,u^._y) g'
+    random g = (V2 x y,g'')
         where (x,g')  = random g
               (y,g'') = random g'
+
+instance Random (f a) => Random (Point f a) where
+    randomR ((P l), (P u)) g = first P $ randomR (l,u) g
+    random g = first P $ random g
                                   
 
-advanceSnake :: Direction -> Snake -> Snake 
-advanceSnake (x,y) s@((x0,y0):_) = (x0+x,y0+y):init s
 
-eatsSelf :: Snake -> Bool
+advanceSnake :: (Num a, Affine p) => Diff p a -> [p a] -> [p a]
+advanceSnake d s@(h:_) = (h.+^d):init s
+
+eatsSelf :: Eq a => [a] -> Bool
 eatsSelf s = nub s /= s
 
-outOfBounds :: Rectangle -> Snake -> Bool
-outOfBounds ((x0,y0),(x1,y1)) ((xs,ys):_) = xs < x0 || ys < y0 || xs > x1 || ys > y1
+outOfBounds :: (Ord a, R2 t, R2 t1, R2 t2) => (t1 a, t2 a) -> [t a] -> Bool
+outOfBounds (u,l) (h:_) = h^._x > u^._x || h^._y < u^._y || h^._x > l^._x || h^._y < l^._y
 
-eatsFood :: Point -> Snake -> Bool
+
+eatsFood :: Eq a => a -> [a] -> Bool
 eatsFood f (h:_) = h == f
 
-advanceGame :: SnakeGame -> SnakeGame 
+advanceGame :: (Num a, Ord a, Random a) => SnakeGame a -> SnakeGame a
 advanceGame game@(SnakeGame snake goal dir score over bounds rng) = 
                 game {snake = newSnake,
                       goal = newGoal,
