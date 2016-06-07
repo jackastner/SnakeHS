43654e5db817908d7c7c786ced4e06bbe3efd2bd
John Kastner <john.h.kastner@gmail.com>
2016-06-07 11:48:08 -0400

    added code for basic keyboard input

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,7 @@ import SDL.Video
 import System.Random
 
 import Control.Monad.State.Lazy
+import Control.Concurrent.Thread.Delay --evil
 
 import Foreign.C.Types
 
@@ -25,5 +26,6 @@ playGame o = do w <- createSnakeWindow
                     advanceGameST
                     drawSnakeGameST r o
                     present r
+                    liftIO $ delay 100000
            
-initialGame =  SnakeGame [P $ V2 0 0] (P $ V2 5 5) (V2 1 1) 0 False (P $ V2 0 0, P $ V2 10 10)  (mkStdGen 0)
+initialGame =  SnakeGame [P $ V2 0 0, P $ V2 0 1, P $ V2 0 2] (P $ V2 5 5) (V2 1 1) 0 False (P $ V2 0 0, P $ V2 10 10)  (mkStdGen 0)
--- src/Snake/EventHandler.hs
+++ src/Snake/EventHandler.hs
@@ -2,16 +2,28 @@ module Snake.EventHandler where
 
 import Snake.GameLogic
 
+import Linear.V2
+
 import SDL.Event
+import SDL.Input.Keyboard.Codes
+import SDL.Input.Keyboard
 
 import Control.Monad.State.Lazy
 
 import System.Exit
 
-handleEventQueue :: MonadIO m => StateT (SnakeGame a) m ()
+handleEventQueue :: (Num a, MonadIO m) => StateT (SnakeGame a) m ()
 handleEventQueue = mapEvents handleEvent
 
 
-handleEvent :: MonadIO m => Event -> StateT (SnakeGame a) m ()
+handleEvent :: (Num a, MonadIO m) => Event -> StateT (SnakeGame a) m ()
 handleEvent (Event _ QuitEvent) = liftIO $ exitSuccess
+handleEvent (Event _ (KeyboardEvent d)) = do 
+    g <- get
+    case keysymKeycode $ keyboardEventKeysym d of 
+        KeycodeLeft -> put $ g {snakeDir = V2 (-1) 0}
+        KeycodeRight -> put $ g {snakeDir = V2 1 0}
+        KeycodeDown -> put $ g {snakeDir = V2 0 1}
+        KeycodeUp -> put $ g {snakeDir = V2 0 (-1)}
+        _ -> return ()
 handleEvent _         = return ()
